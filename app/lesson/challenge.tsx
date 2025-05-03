0cfb90b8aa65f5eb3dfbf9a555c304663aa44991
Chiraagrah <chirag.sharma1701@gmail.com>
2025-05-03 17:15:19 +0530

    fix: correct string interpolation for shortcut in Challenge component

--- app/lesson/challenge.tsx
+++ app/lesson/challenge.tsx
@@ -32,7 +32,7 @@ export const Challenge =({
                     id={option.id}
                     text={option.text}
                     imageSrc={option.imageSrc}
-                    shortcut={'${i+1}'}
+                    shortcut={`${i+1}`}
                     selected={selectedOption === option.id}
                     onClick={()=> onSelect(option.id)}
                     status = {status}
