3d016b4c5a5bd9e6da83e40040c50db772e3740d
Chiraagrah <chirag.sharma1701@gmail.com>
2025-05-05 01:51:00 +0530

    feat: Add Separator component and integrate it into the Leaderboard page

--- app/(main)/leaderboard/page.tsx
+++ app/(main)/leaderboard/page.tsx
@@ -1,4 +1,6 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { FeedWrapper } from "@/components/ui/feed-wrapper";
+import { Separator } from "@/components/ui/separator";
 import { StickyWrapper } from "@/components/ui/sticky-wrapper";
 import { UserProgress } from "@/components/ui/user-progress";
 import { getTopTenUsers, getUserProgress, getUserSubscription } from "@/db/queries";
@@ -51,9 +53,30 @@ const LeaderboardPage = async() => {
                     <p className="text-muted-foreground text-center text-lg mb-6">
                         See where you stand among other learners in the community,
                     </p>
+                    <Separator className="mb-4 h-0.5 rounded-full"/>
                     {leaderboard.map((userProgress,index)=>(
-                            <div key={userProgress.userId}>
+                            <div 
+                                key={userProgress.userId}
+                                className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
+                            >
+                                <p className="font-bold text-lime-700 mr-4">
+                                    {index + 1}
+                                </p>
+                                <Avatar 
+                                    className="border bg-green-500 h-12 w-12 ml-3 mr-6"
+                                >
+                                    <AvatarImage
+                                        className="object-cover" 
+                                        src={userProgress.userImageSrc}
+                                    />
+                                </Avatar>
+                                <p className="font-bold text-neutral-800 flex-1">
                                     {userProgress.userName}
+                                </p>
+                                <p className="text-muted-foreground">
+                                    {userProgress.points} XP
+                                </p>
+                                    
                             </div>
                     ))}
                 </div> 
